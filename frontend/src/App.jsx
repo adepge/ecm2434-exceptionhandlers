b425ceba56ed77ad8d505b3b8bcddd871df14bb8
lyca499 <lyca499abc@gmail.com>
2024-02-12 20:36:29 +0000

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
 import Header from "./features/header";
 import Footer from "./features/footer";
 
+import MapPage from "./pages/map";
+import FeedPage from "./pages/feed";
+
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<MapPage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        {/* Define other routes here */}
+      </Routes>
       <Footer />
     </>
   );
 }
-
 export default App;
--- frontend/src/features/footer.jsx
+++ frontend/src/features/footer.jsx
@@ -5,29 +5,36 @@ import topicon from "../assets/footer/top.svg";
 import captureicon from "../assets/footer/capture.svg";
 
 import "./stylesheets/footer.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer>
-      <div id="map" className="button">
-        <div className="backdrop">
-          <img src={mapicon} id="map-icon" alt="map-icon" />
-        </div>
-        <div className="footer-text">map</div>
+      <div className="button">
+        <Link to="/">
+          <div className="backdrop">
+            <img src={mapicon} id="map-icon" alt="map-icon" />
+          </div>
+          <div className="footer-text">map</div>
+        </Link>
       </div>
-      <div id="map" className="button">
-        <div className="backdrop">
-          <img src={feedicon} id="feed-icon" alt="feed-icon" />
-        </div>
-        <div className="footer-text">feed</div>
+      <div className="button">
+        <Link to="/feed">
+          <div className="backdrop">
+            <img src={feedicon} id="feed-icon" alt="feed-icon" />
+          </div>
+          <div className="footer-text">feed</div>
+        </Link>
       </div>
-      <div id="map" className="button">
-        <div className="backdrop">
-          <img src={topicon} id="top-icon" alt="top-icon" />
-        </div>
-        <div className="footer-text">top</div>
+      <div className="button">
+        <Link to="/top">
+          <div className="backdrop">
+            <img src={topicon} id="top-icon" alt="top-icon" />
+          </div>
+          <div className="footer-text">top</div>
+        </Link>
       </div>
-      <div id="map" className="button">
+      <div className="button">
         <div className="backdrop">
           <img src={captureicon} id="capture-icon" alt="capture-icon" />
         </div>
