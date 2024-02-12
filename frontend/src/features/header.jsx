ba5ec98872b48b4121f322a787fb8abe2eaf658d
lyca499 <lyca499abc@gmail.com>
2024-02-12 18:29:37 +0000

    added folders

---
+++ frontend/src/features/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import icon from "../assets/icon.svg";
+import "./stylesheets/header.css";
+
+function Header() {
+  return (
+    <header>
+      <div id="logo">
+        <img src={icon} id="icon" alt="icon" />
+        <div id="title">Napolean finder</div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
