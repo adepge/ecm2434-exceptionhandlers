456c8af5b65fb2fdb8d7f1b8360f99b4bd32660e
lyca499 <lyca499abc@gmail.com>
2024-02-12 21:41:40 +0000

    footer

--- frontend/src/features/footer.jsx
+++ frontend/src/features/footer.jsx
@@ -3,16 +3,24 @@ import mapicon from "../assets/footer/map.svg";
 import feedicon from "../assets/footer/feed.svg";
 import topicon from "../assets/footer/top.svg";
 import captureicon from "../assets/footer/capture.svg";
+import { useLocation } from "react-router-dom";
 
 import "./stylesheets/footer.css";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
+  const location = useLocation();
+
   return (
     <footer>
       <div className="button">
         <Link to="/">
-          <div className="backdrop">
+          <div
+            className="backdrop"
+            style={{
+              background: location.pathname === "/" ? "#00DCA5" : "none",
+            }}
+          >
             <img src={mapicon} id="map-icon" alt="map-icon" />
           </div>
           <div className="footer-text">map</div>
@@ -20,7 +28,12 @@ export default function Footer() {
       </div>
       <div className="button">
         <Link to="/feed">
-          <div className="backdrop">
+          <div
+            className="backdrop"
+            style={{
+              background: location.pathname === "/feed" ? "#00DCA5" : "none",
+            }}
+          >
             <img src={feedicon} id="feed-icon" alt="feed-icon" />
           </div>
           <div className="footer-text">feed</div>
@@ -28,14 +41,24 @@ export default function Footer() {
       </div>
       <div className="button">
         <Link to="/top">
-          <div className="backdrop">
+          <div
+            className="backdrop"
+            style={{
+              background: location.pathname === "/top" ? "#00DCA5" : "none",
+            }}
+          >
             <img src={topicon} id="top-icon" alt="top-icon" />
           </div>
           <div className="footer-text">top</div>
         </Link>
       </div>
       <div className="button">
-        <div className="backdrop">
+        <div
+          className="backdrop"
+          style={{
+            background: location.pathname === "/capture" ? "#00DCA5" : "none",
+          }}
+        >
           <img src={captureicon} id="capture-icon" alt="capture-icon" />
         </div>
         <div className="footer-text">capture</div>
