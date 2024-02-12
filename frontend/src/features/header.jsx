ef05e4fefdf5950f65394a9e6a9d4f139b3b11f3
lyca499 <lyca499abc@gmail.com>
2024-02-12 18:54:34 +0000

    change header

--- frontend/src/features/header.jsx
+++ frontend/src/features/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import icon from "../assets/icon.svg";
+import usericon from "../assets/user-icon.svg";
 import "./stylesheets/header.css";
 
 function Header() {
@@ -9,6 +10,7 @@ function Header() {
         <img src={icon} id="icon" alt="icon" />
         <div id="title">Napoleon finder</div>
       </div>
+      <img src={usericon} id="user-icon" alt="user-icon" />
     </header>
   );
 }
