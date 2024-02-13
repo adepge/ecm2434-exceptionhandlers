e26a24ed72a630d2f3c09fc0fa8794f77fbcb3bc
adamg-ht135 <adepge@gmail.com>
2024-02-13 15:46:51 +0000

    Add map page

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import './stylesheets/map.css';
 
 function MapPage() {
+  const position = {lat: 50.735424, lng: -3.534504};
+
   return (
-    <div>
-      <h1>Map Page</h1>
-    </div>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <div className="mapContainer">
+      <Map
+      defaultCenter={position}
+      defaultZoom={17}
+      gestureHandling={'greedy'}
+      disableDefaultUI={true}
+    />
+      </div>
+    </APIProvider>
   );
 }
 
