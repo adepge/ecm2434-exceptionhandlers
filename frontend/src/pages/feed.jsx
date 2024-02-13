66678e2ce55fb8f05b1a20dd64b566baaa1fab94
lyca499 <lyca499abc@gmail.com>
2024-02-13 09:12:57 +0000

    update feed

--- frontend/src/pages/feed.jsx
+++ frontend/src/pages/feed.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "./stylesheets/feed.css";
+import { useState } from "react";
+import image1 from "../assets/feed/image1.png";
+import image2 from "../assets/feed/image2.png";
+import image3 from "../assets/feed/image3.png";
+import image4 from "../assets/feed/image4.png";
 
 function FeedPage() {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <div>
-      <h1>Feed Page</h1>
+    <div id="feed">
+      <div id="top">
+        <img
+          src={image4}
+          alt="image1"
+          onClick={toggleClass}
+          className={isActive ? "active" : null}
+        />
+        <div className="interactives">
+          <div className="like">like</div>
+        </div>
+      </div>
     </div>
   );
 }
