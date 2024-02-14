9c22186b20b89dfa52c9eb6e310d4be4aad951eb
lyca499 <lyca499abc@gmail.com>
2024-02-14 20:15:22 +0000

    added like button animation

--- frontend/src/features/interactives.jsx
+++ frontend/src/features/interactives.jsx
@@ -14,9 +14,9 @@ function Interactives({ likes, location, userIcon, liked, likeFunction }) {
     // prevent clicking the background when the like button is clicked from triggering the background click event
     event.stopPropagation();
     if (vliked) {
-      vsetLikes(vlikes + 1);
-    } else {
       vsetLikes(vlikes - 1);
+    } else {
+      vsetLikes(vlikes + 1);
     }
     vlikeFunction(!vliked);
   }
@@ -29,11 +29,7 @@ function Interactives({ likes, location, userIcon, liked, likeFunction }) {
           toggleLike(event);
         }}
       >
-        <img
-          src={vliked ? HollowHeart : Heart}
-          height={"14px"}
-          width={"14px"}
-        />
+          <div className={vliked? "icon-inner active": "icon-inner"}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="s-ion-icon"><path d="M256 448l-30.164-27.211C118.718 322.442 48 258.61 48 179.095 48 114.221 97.918 64 162.4 64c36.399 0 70.717 16.742 93.6 43.947C278.882 80.742 313.199 64 349.6 64 414.082 64 464 114.221 464 179.095c0 79.516-70.719 143.348-177.836 241.694L256 448z"></path></svg></div>
         {vlikes}
       </div>
       <div className="location element">
