3b31b31f9386175ef527459e078a7dc0c20ef849
lyca499 <lyca499abc@gmail.com>
2024-02-14 10:36:05 +0000

    making header fixed and reduced spacing between feed

--- frontend/src/features/header.jsx
+++ frontend/src/features/header.jsx
@@ -6,11 +6,13 @@ import "./stylesheets/header.css";
 function Header() {
   return (
     <header>
-      <div id="logo">
-        <img src={icon} id="icon" alt="icon" />
-        <div id="title">Napoleon finder</div>
+      <div id="header-wrapper">
+        <div id="logo">
+          <img src={icon} id="icon" alt="icon" />
+          <div id="title">Napoleon finder</div>
+        </div>
+        <img src={usericon} id="user-icon" alt="user-icon" />
       </div>
-      <img src={usericon} id="user-icon" alt="user-icon" />
     </header>
   );
 }
