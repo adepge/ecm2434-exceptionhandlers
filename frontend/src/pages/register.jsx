6363b2ba2f34bfa65a584f61bcd75636b29f6c53
lyca499 <lyca499abc@gmail.com>
2024-02-19 23:00:01 +0000

    registration page final touch

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -2,20 +2,23 @@ import "./stylesheets/register.css";
 
 function RegisterPage() {
   return (
-    <div id="register-wrapper">
-      <div id="spacing">
+    <div id="display">
+      <div id="register-wrapper">
         <div id="register">
-          <div id="title">Register</div>
-          <div id="form">
-            <input className="text" type="text" placeholder="Username" />
-            <input className="text" type="text" placeholder="Email" />
-            <input className="text" type="password" placeholder="Password" />
-            <input
-              className="text"
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <button>Register</button>
+          <div id="spacer">
+            <div id="title">Register</div>
+            <div id="form">
+              <input className="text" type="text" placeholder="Username" />
+              <input className="text" type="text" placeholder="Email" />
+              <input className="text" type="password" placeholder="Password" />
+              <input
+                className="text"
+                type="password"
+                placeholder="Confirm Password"
+              />
+              <button>Register</button>
+            </div>
+
           </div>
         </div>
       </div>
