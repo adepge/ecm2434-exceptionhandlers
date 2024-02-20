c92ccacc8d5197b459f980da50bb9a03f63b1dc6
ziyad <ziyad.alnawfal1@gmail.com>
2024-02-20 17:10:37 +0000

    login page

--- backend/PostCard/PostCard/wsgi.py
+++ backend/PostCard/PostCard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for catnav project.
+WSGI config for PostCard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catnav.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PostCard.settings')
 
 application = get_wsgi_application()
