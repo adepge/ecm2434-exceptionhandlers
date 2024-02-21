06c9fe824682896590d44928a1ba63f5d10a6ae9
JayantC31 <jayant.chawla31@gmail.com>
2024-02-21 14:55:05 +0000

    Register models for admin

--- backend/PostCard/database/admin.py
+++ backend/PostCard/database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Geolocation, Posts, Stickers, StickersUser
+
+admin.site.register(Geolocation)
+admin.site.register(Posts)
+admin.site.register(Stickers)
+admin.site.register(StickersUser)
