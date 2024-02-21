4b5f270a844f8e9575db54622b6630ce80ed9260
JayantC31 <jayant.chawla31@gmail.com>
2024-02-21 17:28:28 +0000

    added all serializers needed for basic model

--- backend/PostCard/PostCard/urls.py
+++ backend/PostCard/PostCard/urls.py
@@ -16,13 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from database.views import PostsList, PostsDetail
+from database.views import PostsList, PostsDetail, GeolocationList, GeolocationDetail, StickersList, StickersDetail, StickersUserList, StickersUserDetail
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', PostsList.as_view(), name='posts-list'),
     path('posts/<int:pk>/', PostsDetail.as_view(), name='posts-detail'),
+    path('geolocations/', GeolocationList.as_view(), name='geolocation-list'),
+    path('geolocations/<int:pk>/', GeolocationDetail.as_view(), name='geolocation-detail'),
+    
+    # Sticker URLs
+    path('stickers/', StickersList.as_view(), name='sticker-list'),
+    path('stickers/<int:pk>/', StickersDetail.as_view(), name='sticker-detail'),
+
+    # StickerUser URLs
+    path('stickerusers/', StickersUserList.as_view(), name='stickeruser-list'),
+    path('stickerusers/<int:pk>/', StickersUserDetail.as_view(), name='stickeruser-detail'),
 ]
 
 
--- backend/PostCard/database/views.py
+++ backend/PostCard/database/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Posts
-from PostCard.serializers import PostsSerializer
+from .models import Geolocation, Posts, Stickers, StickersUser
+from PostCard.serializers import PostsSerializer, GeolocationSerializer, StickersSerializer, StickersUserSerializer
 
 class PostsList(generics.ListCreateAPIView):
     queryset = Posts.objects.all()
@@ -10,3 +10,27 @@ class PostsList(generics.ListCreateAPIView):
 class PostsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
+
+class GeolocationList(generics.ListAPIView):
+    queryset = Geolocation.objects.all()
+    serializer_class = GeolocationSerializer
+
+class GeolocationDetail(generics.RetrieveAPIView):
+    queryset = Geolocation.objects.all()
+    serializer_class = GeolocationSerializer
+
+class StickersList(generics.ListAPIView):
+    queryset = Stickers.objects.all()
+    serializer_class = StickersSerializer
+
+class StickersDetail(generics.RetrieveAPIView):
+    queryset = Stickers.objects.all()
+    serializer_class = StickersSerializer
+
+class StickersUserList(generics.ListAPIView):
+    queryset = StickersUser.objects.all()
+    serializer_class = StickersUserSerializer
+
+class StickersUserDetail(generics.RetrieveAPIView):
+    queryset = StickersUser.objects.all()
+    serializer_class = StickersUserSerializer
