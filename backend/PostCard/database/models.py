86fd6ec0076a1611112d5f0c1eebf75dc3f0b713
astr0guy <benellison2003@gmail.com>
2024-02-23 00:49:40 +0000

    Ammended prior commit where migrations were modified, instead modified models

--- backend/PostCard/database/models.py
+++ backend/PostCard/database/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User #Importing django provided User mode
 # Each FK is set to on_delete = models.CASCADE to uphold DB integrity and consisentiy
 
 class Geolocation(models.Model):
-    geolocID = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     location = models.CharField(max_length=255)
     latitude = models.FloatField(max_length=255)
     longitude = models.FloatField(max_length=255)
@@ -13,22 +13,21 @@ class Geolocation(models.Model):
     def __str__(self):
         return f"{self.location} (Lat: {self.latitude}, Lng: {self.longitude})"
     
-
-
 class Posts(models.Model):
-    postId   = models.IntegerField()
+    id   = models.AutoField(primary_key=True)
     fileName = models.CharField(max_length = 255)
     geolocID = models.ForeignKey(Geolocation, on_delete = models.CASCADE)
     username = models.ForeignKey(User,        on_delete = models.CASCADE)
     caption  = models.CharField(max_length = 255)
-    datetime = models.DateTimeField(auto_now_add = True) #Creates a timestap
+    datetime = models.DateTimeField(auto_now_add = True) #Creates a timestamp
 
 class Stickers(models.Model):
-    stickersID   = models.IntegerField(primary_key = True)
+    id   = models.AutoField(primary_key=True)
     stickersName = models.CharField(max_length = 50)
     stickersDescription = models.CharField(max_length = 100)
     fileName = models.CharField(max_length = 100)
     
 class StickersUser(models.Model):
-    StickersID = models.ForeignKey(Stickers, on_delete = models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    stickersID = models.ForeignKey(Stickers, on_delete = models.CASCADE)
     username   = models.ForeignKey(User, on_delete = models.CASCADE)
