efc28ce50254e550284d9db974eee721492ada19
JayantC31 <jayant.chawla31@gmail.com>
2024-02-23 01:05:13 +0000

    Fixed database issues

--- backend/PostCard/database/admin.py
+++ backend/PostCard/database/admin.py
@@ -2,17 +2,17 @@ from django.contrib import admin
 from .models import Geolocation, Posts, Stickers, StickersUser
 # Register models to admint page based on the models.py file, adding the list_display and search_fields to make it easier to search and view the data
 class PostsAdmin(admin.ModelAdmin):
-    list_display = ('postId', 'fileName', 'username', 'datetime')
+    list_display = ('id', 'fileName', 'username', 'datetime')
     search_fields = ['fileName', 'username__username']
 
 class GeolocationAdmin(admin.ModelAdmin):
-    list_display = ('geolocID', 'location', 'latitude', 'longitude')
+    list_display = ('id', 'location', 'latitude', 'longitude')
     
 class StickersAdmin(admin.ModelAdmin):
-    list_display = ('stickersID', 'stickersName', 'stickersDescription', 'fileName')
+    list_display = ('id', 'stickersName', 'stickersDescription', 'fileName')
     
 class StickersUserAdmin(admin.ModelAdmin):
-    list_display = ('StickersID', 'username')
+    list_display = ('id', 'username')
     
 # Register to admin site
 admin.site.register(Geolocation, GeolocationAdmin)
