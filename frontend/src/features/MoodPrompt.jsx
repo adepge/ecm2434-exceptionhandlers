26fc192745fc1f7f49195fead8bcd1964856260b
adepge <adepge@gmail.com>
2024-02-24 17:16:24 +0000

    Added res files for map

--- frontend/src/features/MoodPrompt.jsx
+++ frontend/src/features/MoodPrompt.jsx
@@ -1,14 +1,17 @@
+import close from '../assets/map/close.svg';
 
-
-function MoodPrompt() {
+function MoodPrompt({onClickFunction}) {
     return (
       <div className="mood-prompt">
         <div id="mood-prompt-card">
-          <button style={{backgroundColor: "#35B39A"}}>Grateful</button>
-          <button>Happy</button>
-          <button>Anxious</button>
-          <button>Tired</button>
-          <button>Down</button>
+          <img onClick={() => onClickFunction('none')} src={close}/>
+          <p>How are you feeling today?</p>
+          <button onClick={() => onClickFunction('grateful')} style={{backgroundColor: "var(--grateful)"}}>Grateful</button>
+          <button onClick={() => onClickFunction('happy')} style={{backgroundColor: "var(--happy)"}}>Happy</button>
+          <button onClick={() => onClickFunction('anxious')} style={{backgroundColor: "var(--anxious)"}}>Anxious</button>
+          <button onClick={() => onClickFunction('tired')} style={{backgroundColor: "var(--tired)"}}>Tired</button>
+          <button onClick={() => onClickFunction('down')} style={{backgroundColor: "var(--down)"}}>Down</button>
+          <div className="urgent-help">I need urgent help</div>
         </div>
       </div>
     );
--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,5 +1,5 @@
 import {APIProvider, Map, AdvancedMarker, Marker, Pin, InfoWindow} from '@vis.gl/react-google-maps';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {differenceInDays, format} from 'date-fns';
 import './stylesheets/map.css';
 import napoleon from '../assets/map/napoleon.svg'
@@ -14,13 +14,26 @@ import adam from '../assets/profiles/adam.png'
 
 function MapPage() {
 
+  const [showMoodPrompt, setShowMoodPrompt] = useState(false);
+  const [mood, setMood] = useState('unselected');
+
   // Local state, to be replaced with fetched data
   const [locations, setLocations] = useState([
-    {id: 1, position: {lat: 50.735424, lng: -3.534504}, tag: "Great Hall", image: image1, date: '2024-02-15T16:53:21', likes: 4, liked: false, profileImage: jay, open: false},
-    {id: 2, position: {lat: 50.73515821650297, lng: -3.5374750416490075}, tag: "Forum Hill", image: image2, date: '2024-02-14T15:23:02', likes: 12, liked: false, profileImage: jay, open: false},
-    {id: 3, position: {lat: 50.73570670084978, lng: -3.532460585178674}, tag: "Reed Hall", image: image3, date: '2024-02-13T09:58:33', likes: 7, liked: false, profileImage: adam, open: false},
-    {id: 4, position: {lat: 50.736341371401544, lng: -3.5391262256961586}, tag: "Mardon Hill", image: image3, date: '2024-02-13T07:25:41', likes: 25, liked: false, profileImage: adam, open: false},
+    {id: 1, position: {lat: 50.735424, lng: -3.534504}, caption: "This is what I strive for", image: image1, date: '2024-02-15T16:53:21', open: false},
+    {id: 2, position: {lat: 50.73515821650297, lng: -3.5374750416490075}, caption: "Caption 2", image: image2, date: '2024-02-14T15:23:02', open: false},
+    {id: 3, position: {lat: 50.73570670084978, lng: -3.532460585178674}, caption: "Caption 3", image: image3, date: '2024-02-13T09:58:33', open: false},
+    {id: 4, position: {lat: 50.736341371401544, lng: -3.5391262256961586}, caption: "Caption 4", image: image3, date: '2024-02-13T07:25:41', open: false},
   ])
+
+  // Checks if mood has been set before, if not call mood prompt;
+  useEffect(() => {
+    if (sessionStorage.mood === undefined) {
+      setShowMoodPrompt(true);
+    } else {
+      setMood(sessionStorage.mood);
+    }
+  },[])
+
 
   const handleOpen = (id) => {
     setLocations(locations.map(location => {
@@ -31,12 +44,18 @@ function MapPage() {
       }
    }))}
 
+  const handleMoodPrompt = (mood) => {
+    setMood(mood);
+    sessionStorage.mood = mood;
+    setShowMoodPrompt(false);
+  }
+
   const position = {lat: 50.735424, lng: -3.534504};
 
   return (
     <>
-      {/* <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        <div className="mapContainer">
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <div className={showMoodPrompt ? "mapContainer background-blur" : "mapContainer"}>
         <Map
         defaultCenter={position}
         defaultZoom={17}
@@ -46,25 +65,24 @@ function MapPage() {
         >
           {locations.map((location) => {
             const opacityDiff = differenceInDays(new Date(), new Date(location.date)) * 0.142857
-            const color = `rgb(0, 220, 165, ${1 - opacityDiff})`
+            const color = `rgb(0, 220, 165)`
             return (
             <>
               <AdvancedMarker key={location.id} position={location.position} onClick={() => handleOpen(location.id)}>
                 <Pin
                   background={color}
                   borderColor={color}
-                  glyphColor={color}
-                  scale={1.4}
+                  glyphColor="white"
+                  scale={0.8}
                 ></Pin>
               </AdvancedMarker>
-              {location.open && (<InfoWindow className="custom-marker" position={location.position}></InfoWindow>)}
             </>
             )
           })}
         </Map>
         </div>
-      </APIProvider> */}
-      <MoodPrompt />
+      </APIProvider>
+      {showMoodPrompt && <MoodPrompt onClickFunction={handleMoodPrompt} />}
     </>
   );
 }
