5846a622741bc51e6984b5594b8de0b6c2ebf50c
lyca499 <lyca499abc@gmail.com>
2024-02-25 01:40:38 +0000

    completely fixed the problem

--- frontend/src/features/DrawerDown.jsx
+++ frontend/src/features/DrawerDown.jsx
@@ -2,47 +2,50 @@ import { useState, useEffect, useRef } from "react";
 import Polaroid from "./polaroid";
 import handle from "../assets/map/handle.svg";
 
-function DrawerDown({image, drawerVisible, setDrawerVisible}) {
+function DrawerDown({ image, drawerVisible, setDrawerVisible }) {
   const elementRef = useRef(null);
   const [closing, setClosing] = useState(setDrawerVisible);
 
   useEffect(() => {
-    document.addEventListener('click', handleClick);
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  },[]);
+    if (drawerVisible) {
+      setTimeout(() => {
+        document.addEventListener("click", handleClick);
+      }, 650);
+    }
+  }, [drawerVisible]);
 
   const handleClick = (event) => {
     // Check if the clicked element is outside the element
-    
+
     event.preventDefault();
 
     if (elementRef.current && !elementRef.current.contains(event.target)) {
       setClosing(true);
+      document.removeEventListener("click", handleClick);
       setTimeout(() => {
         setDrawerVisible(false);
         setClosing(false);
       }, 650);
-
     }
   };
 
-  const drawerClass = closing ? "drawer-container" : "drawer-container drawer-exit-top";
+  const drawerClass = closing
+    ? "drawer-container"
+    : "drawer-container drawer-exit-top";
 
   return (
     <>
-      {drawerVisible &&
-      <div className={drawerClass} ref={elementRef}>
-        <div id="texture">
-          <Polaroid id="collect-polaroid" src={image} rotation={-5} />
+      {drawerVisible && (
+        <div className={drawerClass} ref={elementRef}>
+          <div id="texture">
+            <Polaroid id="collect-polaroid" src={image} rotation={-5} />
+          </div>
+          <button>Add to collection</button>
+          <img id="handle" src={handle}></img>
         </div>
-        <button>Add to collection</button>
-        <img id="handle" src={handle}></img>
-      </div>}
+      )}
     </>
   );
 }
-  
+
 export default DrawerDown;
-  
