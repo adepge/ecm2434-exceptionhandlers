b4ad4fec330fe8be64d8b4f1a6951f9bd5d4a7da
lyca499 <lyca499abc@gmail.com>
2024-02-25 14:53:08 +0000

    added close bracket in map

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,12 +1,19 @@
-import {APIProvider, Map, AdvancedMarker, Marker, Pin, useMap} from '@vis.gl/react-google-maps';
-import {useState, useEffect, useMemo} from 'react';
-import {differenceInDays, format, set} from 'date-fns';
-import {PathLayer} from '@deck.gl/layers';
-import {GoogleMapsOverlay} from '@deck.gl/google-maps';
-import './stylesheets/map.css';
-import napoleon from '../assets/map/napoleon.svg'
-import MoodPrompt from '../features/MoodPrompt';
-import DrawerDown from '../features/DrawerDown';
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Marker,
+  Pin,
+  useMap,
+} from "@vis.gl/react-google-maps";
+import { useState, useEffect, useMemo } from "react";
+import { differenceInDays, format, set } from "date-fns";
+import { PathLayer } from "@deck.gl/layers";
+import { GoogleMapsOverlay } from "@deck.gl/google-maps";
+import "./stylesheets/map.css";
+import napoleon from "../assets/map/napoleon.svg";
+import MoodPrompt from "../features/MoodPrompt";
+import DrawerDown from "../features/DrawerDown";
 
 // Placeholder imports
 const image1 =
@@ -17,18 +24,17 @@ const image3 =
   "https://cdn.discordapp.com/attachments/1204728741230809098/1207497298116874311/1000016354.JPG?ex=65dfdc7e&is=65cd677e&hm=e497673ab0de533871fc5fb4bb6e702ce4fbaa856f99461dc3abf555c6f0d510&";
 
 // Overlay constructor component (from deck.gl documentation)
-export const DeckGlOverlay = ({layers}) => {
-  const deck = useMemo(() => new GoogleMapsOverlay({interleaved: true}), []);
+export const DeckGlOverlay = ({ layers }) => {
+  const deck = useMemo(() => new GoogleMapsOverlay({ interleaved: true }), []);
 
   const map = useMap();
   useEffect(() => deck.setMap(map), [map]);
-  useEffect(() => deck.setProps({layers}), [layers]);
+  useEffect(() => deck.setProps({ layers }), [layers]);
 
   return null;
 };
 
 function MapPage() {
-  
   // State for mood prompt
   const [showMoodPrompt, setShowMoodPrompt] = useState(false);
   const [mood, setMood] = useState("unselected");
@@ -41,15 +47,43 @@ function MapPage() {
   const [path, setPath] = useState([]);
   const [walking, setWalking] = useState(false);
   const [watchId, setWatchId] = useState(null);
-  const [position, setPosition] = useState({lat: 50.735850, lng: -3.533415});
-  
+  const [position, setPosition] = useState({ lat: 50.73585, lng: -3.533415 });
+
   // Local state, to be replaced with fetched data
   const [locations, setLocations] = useState([
-    {id: 1, position: {lat: 50.735224, lng: -3.536504}, caption: "This is what I strive for", image: image1, date: '2024-02-15T16:53:21', open: false},
-    {id: 2, position: {lat: 50.73515821650297, lng: -3.5374750416490075}, caption: "Caption 2", image: image2, date: '2024-02-14T15:23:02', open: false},
-    {id: 3, position: {lat: 50.73570670084978, lng: -3.532460585178674}, caption: "Caption 3", image: image3, date: '2024-02-13T09:58:33', open: false},
-    {id: 4, position: {lat: 50.736341371401544, lng: -3.5391262256961586}, caption: "Caption 4", image: image3, date: '2024-02-13T07:25:41', open: false},
-  ])
+    {
+      id: 1,
+      position: { lat: 50.735224, lng: -3.536504 },
+      caption: "This is what I strive for",
+      image: image1,
+      date: "2024-02-15T16:53:21",
+      open: false,
+    },
+    {
+      id: 2,
+      position: { lat: 50.73515821650297, lng: -3.5374750416490075 },
+      caption: "Caption 2",
+      image: image2,
+      date: "2024-02-14T15:23:02",
+      open: false,
+    },
+    {
+      id: 3,
+      position: { lat: 50.73570670084978, lng: -3.532460585178674 },
+      caption: "Caption 3",
+      image: image3,
+      date: "2024-02-13T09:58:33",
+      open: false,
+    },
+    {
+      id: 4,
+      position: { lat: 50.736341371401544, lng: -3.5391262256961586 },
+      caption: "Caption 4",
+      image: image3,
+      date: "2024-02-13T07:25:41",
+      open: false,
+    },
+  ]);
 
   // Sample data for path layer (to be replaced as well)
   const path2 = [
@@ -73,21 +107,20 @@ function MapPage() {
         [-3.532825, 50.735802],
         [-3.532825, 50.735802],
         [-3.533136, 50.735856],
-        [-3.533415, 50.735850],
+        [-3.533415, 50.73585],
       ],
-    }
+    },
   ];
 
   // Layer constructor for path layer (from deck.gl documentation)
-  const layer = 
-    new PathLayer({
-      id: 'path-layer',
-      data: path2,
-      getPath: d => d.path,
-      getColor: [73, 146, 255],
-      getWidth: 7,
-      widthMinPixels: 2
-    });
+  const layer = new PathLayer({
+    id: "path-layer",
+    data: path2,
+    getPath: (d) => d.path,
+    getColor: [73, 146, 255],
+    getWidth: 7,
+    widthMinPixels: 2,
+  });
 
   // Checks if mood has been set before, if not call mood prompt;
   useEffect(() => {
@@ -97,8 +130,6 @@ function MapPage() {
       setMood(sessionStorage.mood);
     }
   }, []);
-  },[])
-
 
   // Get user's location
   // useEffect(() => {
@@ -152,46 +183,55 @@ function MapPage() {
         setDrawerVisible={setDrawerTopVisible}
       />
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        <div className={showMoodPrompt ? "mapContainer background-blur" : "mapContainer"}>
-        <Map
-        id="map"
-        defaultCenter={position}
-        defaultZoom={17}
-        gestureHandling={'greedy'}
-        disableDefaultUI={true}
-        mapId={import.meta.env.VITE_GOOGLE_MAPS_MAP_ID}
-        >   
-          <DeckGlOverlay layers={layer} />
-          <AdvancedMarker position={position}>
-            <div
-              style={{
-                width: 16,
-                height: 16,
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                background: '#4185f5',
-                border: '2px solid #ffffff',
-                borderRadius: '50%',
-                transform: 'translate(-50%, -50%)'
-              }}></div>
-          </AdvancedMarker>
-          {locations.map((location) => {
-            const color = `var(--${mood})`
-            return (
-            <>
-              <AdvancedMarker key={location.id} position={location.position} onClick={(e) => handleOpen(e,location.id)}>
-                <Pin
-                  background={color}
-                  borderColor={color}
-                  glyphColor="white"
-                  scale={0.8}
-                ></Pin>
-              </AdvancedMarker>
-            </>
-            )
-          })}
-        </Map>
+        <div
+          className={
+            showMoodPrompt ? "mapContainer background-blur" : "mapContainer"
+          }
+        >
+          <Map
+            id="map"
+            defaultCenter={position}
+            defaultZoom={17}
+            gestureHandling={"greedy"}
+            disableDefaultUI={true}
+            mapId={import.meta.env.VITE_GOOGLE_MAPS_MAP_ID}
+          >
+            <DeckGlOverlay layers={layer} />
+            <AdvancedMarker position={position}>
+              <div
+                style={{
+                  width: 16,
+                  height: 16,
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  background: "#4185f5",
+                  border: "2px solid #ffffff",
+                  borderRadius: "50%",
+                  transform: "translate(-50%, -50%)",
+                }}
+              ></div>
+            </AdvancedMarker>
+            {locations.map((location) => {
+              const color = `var(--${mood})`;
+              return (
+                <>
+                  <AdvancedMarker
+                    key={location.id}
+                    position={location.position}
+                    onClick={(e) => handleOpen(e, location.id)}
+                  >
+                    <Pin
+                      background={color}
+                      borderColor={color}
+                      glyphColor="white"
+                      scale={0.8}
+                    ></Pin>
+                  </AdvancedMarker>
+                </>
+              );
+            })}
+          </Map>
         </div>
       </APIProvider>
       {showMoodPrompt && <MoodPrompt onClickFunction={handleMoodPrompt} />}
