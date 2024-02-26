67f93313b4681715df6a48c06e2a2aa894eb0d73
ziyad <ziyad.alnawfal1@gmail.com>
2024-02-26 03:47:28 +0000

    included token generation for registration

--- backend/PostCard/user_authentication/views.py
+++ backend/PostCard/user_authentication/views.py
@@ -15,20 +15,23 @@ from django.contrib.auth.models import User
 def UserRegisterAuthentication(request):
     serializer = UserRegisterSerializer(data=request.data)
     if serializer.is_valid():
+
         username = serializer.validated_data.get('username')
         email = serializer.validated_data.get('email')
         password = serializer.validated_data.get('password')
         user_instance = User(username=username,email=email)
-        user_instance.set_password(password)
+        user_instance.set_password(password) # Must use set password so django recoginzes its a password
         user_instance.save()
+        token, _ = Token.objects.get_or_create(user=user_instance) # Avoids needing to login in after making page
+
         
-        return Response({"message":"User made"},status=status.HTTP_201_CREATED)    # Successful user creation
+        return Response({f"message": token.key},status=status.HTTP_200_OK)    # Successful user creation
     else: 
-        return Response(status=status.HTTP_400_BAD_REQUEST) # Failed user creation
+        return Response(status=status.HTTP_400_BAD_REQUEST)                   # Failed user creation
 
 
 @api_view(['POST']) # Secuirty purposes we dont want to append user details to header
-@permission_classes([AllowAny]) # idk , doesnt work without it smh
+@permission_classes([AllowAny]) 
 def UserLoginAuthentication(request):
     username = request.data.get('username')
     password = request.data.get('password')
