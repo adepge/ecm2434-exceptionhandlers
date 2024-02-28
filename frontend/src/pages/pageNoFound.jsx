353de204de15c6e7e8942b64aeddf5caac3afa2d
adepge <adepge@gmail.com>
2024-02-28 02:28:12 +0000

    Fixed conflicting css classes

--- frontend/src/pages/pageNoFound.jsx
+++ frontend/src/pages/pageNoFound.jsx
@@ -3,10 +3,10 @@ import buddy from "../assets/404buddy.jpg"
 
 function PageNoFound() {
   return (
-    <div id="display">
-      <img src={buddy}></img>
-      <div id="status">404</div>
-      <p id="description">Uh oh! We can't find the page you were looking for</p>
+    <div id="display-not-found">
+      <img id="not-found-img" src={buddy}></img>
+      <div id="status-not-found">404</div>
+      <p id="description-not-found">Uh oh! We can't find the page you were looking for</p>
     </div>
   );
 }
