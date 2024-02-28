0057570fd4d22d9314a4626dd461bf94ebec0da3
adepge <adepge@gmail.com>
2024-02-28 02:23:41 +0000

    Added illustration to 404 page

--- frontend/src/pages/pageNoFound.jsx
+++ frontend/src/pages/pageNoFound.jsx
@@ -1,10 +1,12 @@
 import "./stylesheets/pageNoFound.css";
+import buddy from "../assets/404buddy.jpg"
 
 function PageNoFound() {
   return (
     <div id="display">
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
+      <img src={buddy}></img>
+      <div id="status">404</div>
+      <p id="description">Uh oh! We can't find the page you were looking for</p>
     </div>
   );
 }
