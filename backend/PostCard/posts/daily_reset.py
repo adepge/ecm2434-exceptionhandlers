1b69eb7e3e951a00a8836dae0b6e45494f8f06e2
astr0guy <benellison2003@gmail.com>
2024-03-05 23:27:55 +0000

    Fixed daily reset (now checks for date integrity and works in general)

--- backend/PostCard/posts/daily_reset.py
+++ backend/PostCard/posts/daily_reset.py
@@ -1,22 +1,28 @@
-from background_task import background
 import random
+import datetime
+import pytz
 
-@background(schedule=60)
 def dailyReset():
-    random.seed()
     # Import the Employee model
-    from database.models import PostsUser, Challenges
-    for i in PostsUser.objects.all():
-        x=PostsUser.objects.all()[i]
-        x.stepsTakenToday=0
-        x.postsMadeToday=0
-        x.postsSavedToday=0
-        x.save()
-    y=Challenges.objects.all()[0:9]
-    for j in y:
-        j.inUse=False
-    y.save()
-    z=Challenges.objects.all()[random.randint(1,10)]
-    z.inUse=True
-    z.save()
-    print("DATABASE RESET")
+    from database.models import PostsUser, Challenges, CurrentDay
+    date = CurrentDay.objects.all()[0]
+    timezone = pytz.timezone('Europe/London')
+    currentDate = datetime.now(timezone).date()
+    if currentDate != date.date:
+        random.seed()
+        for i in PostsUser.objects.all():
+            x=PostsUser.objects.all()[i]
+            x.stepsTakenToday=0
+            x.postsMadeToday=0
+            x.postsSavedToday=0
+            x.save()
+        y=Challenges.objects.all()[0:9]
+        for j in y:
+            j.inUse=False
+        y.save()
+        z=Challenges.objects.all()[random.randint(1,10)]
+        z.inUse=True
+        z.save()
+        date.dateOfLastInteraction = currentDate
+        print("DAILY RESET SUCCESSFUL")
+    print("DAILY RESET NOT NEEDED")
