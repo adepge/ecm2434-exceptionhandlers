a0a502935742b1bd178f3101862290f1f2df4ada
lyca499 <lyca499abc@gmail.com>
2024-03-14 18:12:36 +0000

    test for changeProfile

--- frontend/src/pages/test.jsx
+++ frontend/src/pages/test.jsx
@@ -17,9 +17,14 @@ function Test() {
     e.preventDefault();
     try {
       // Update the API URL as per your configuration
-      const response = await axios.get(
-        "http://127.0.0.1:8000/api/checkWinner/"
-        ,
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/changeProfile/"
+        , {
+          bio: "changedBio",
+          youtube: "changedYoutube",
+          twitter: "changedTwitter",
+          instagram: "changedInstagram",
+        },
         {
           headers: {
             "Content-Type": "application/json",
