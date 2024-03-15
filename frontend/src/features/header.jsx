2f2065fa531606eeac883e568974518d865ffcc9
lyca499 <lyca499abc@gmail.com>
2024-03-15 21:03:17 +0000

    fixed header logout

--- frontend/src/features/header.jsx
+++ frontend/src/features/header.jsx
@@ -70,7 +70,7 @@ function Header() {
       );
      
       cookies.remove('token');
-      location.reload();
+      window.location.reload();
     } catch (error) {
       console.error("Error occurred:", error);
       if (error.response) {
