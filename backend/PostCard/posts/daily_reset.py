2e27a78f42a405379801789023de6d7942d82a00
ziyad <ziyad.alnawfal1@gmail.com>
2024-03-18 15:11:22 +0000

    included comments and modified dailyReset()

--- backend/PostCard/posts/daily_reset.py
+++ backend/PostCard/posts/daily_reset.py
@@ -1,5 +1,5 @@
 
-
+# function that resets user info related to all daily challenges and actives a random daily challenge
 def dailyReset():
     try:
         import random
@@ -11,20 +11,27 @@ def dailyReset():
         print("CURRENT DATE ACCORDING TO DEVICE = " + str(currentDate))
         date,_ = CurrentDay.objects.get_or_create()
         print("CURRENT DATE ACCORDING TO DATABASE = " + str(date.dateOfLastInteraction))
+
+        # checking if the daily challenge has expired, (24hrs)
         if currentDate != date.dateOfLastInteraction:
             random.seed()
+            # reseting user info for daily challenges
             for x in PostsUser.objects.all():
                 x.stepsTakenToday=0
                 x.postsMadeToday=0
                 x.postsSavedToday=0
                 x.save()
             
-            for y in Challenges.objects.all()[0:1]:
+            # Resets all daily challenges to inactive and actives a random one
+            number_daily_challenges=0
+            for y in Challenges.objects.filter(type="daily"):
                 y.inUse=False
+                number_daily_challenges+=1
                 y.save()
-            z=Challenges.objects.all()[random.randint(0,1)]
+            z=Challenges.objects.filter(type="daily")[random.randint(0,number_daily_challenges)]
             z.inUse=True
             z.save()
+            
             date.dateOfLastInteraction = currentDate
             date.save()
             print("DAILY RESET SUCCESSFUL")
