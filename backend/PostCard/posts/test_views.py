738c2ca004ad3e0e3e1a6ea348ebccb95174845c
JayantC31 <jayant.chawla31@gmail.com>
2024-03-18 16:56:07 +0000

    add another test

--- backend/PostCard/posts/test_views.py
+++ backend/PostCard/posts/test_views.py
@@ -5,6 +5,8 @@ from rest_framework.test import APIClient
 from database.models import Geolocation, Posts, Stickers, PostsUser, Challenges
 from django.contrib.auth.models import User
 from .views import *
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 # USE python  manage.py test --pattern="test_views.py"
 class PostCardTests(TestCase):
     def setUp(self):
@@ -14,6 +16,11 @@ class PostCardTests(TestCase):
         # Create test users
         self.user1 = User.objects.create_user(username='user1', password='pass')
         self.user2 = User.objects.create_user(username='user2', password='pass')
+        self.geolocation = Geolocation.objects.create(location='Test Location', latitude=10.0, longitude=20.0)
+        
+        # Adjust the login method according to your authentication setup
+        #self.client.force_authenticate(user=self.user1)
+
 
         # Create test data for models
         self.geolocation = Geolocation.objects.create(location='Test Location', latitude=10.0, longitude=20.0)
@@ -28,39 +35,24 @@ class PostCardTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
-    def test_get_user(self):
-        # Test getting a user profile
-        self.client.force_authenticate(user=self.user1)
-        response = self.client.get(reverse('get-user'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['username'], 'user1')
+    def test_create_post(self):
+    # Use a real image file's path or include a valid image file in your test directory
+        image_path = r'./media/media/Avatar/crown.png'
+        with open(image_path, 'rb') as img:
+            image_data = img.read()
+        
+        image = SimpleUploadedFile('test_image.png', image_data, content_type='image/png')
+        data = {
+            'userid': self.user1.id,  # Use the correct field name and dynamically set the value
+            'caption': 'A new post',
+            'geolocID': self.geolocation.id,
+            'image': image,
+        }
 
-    # def test_create_post(self):
-    #     # Test creating a post
-    #     self.client.force_authenticate(user=self.user1)
-    #     data = {'caption': 'New Post', 'geolocID': self.geolocation.id, 'image': 'backend\PostCard\media\media\Avatar\crown.png'}
-    #     response = self.client.post(reverse('create-post'), data, format='multipart')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.client.force_authenticate(user=self.user1)  # Ensure authentication is set
+        response = self.client.post(reverse('posts-list'), data, format='multipart')
+        
+        print(response.data)  # To debug in case of errors
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    # def test_purchase_sticker(self):
-    #     # Test purchasing a sticker
-    #     self.client.force_authenticate(user=self.user1)
-    #     data = {'sticker': 'Test Sticker'}
-    #     response = self.client.post(reverse('purchase-sticker'), data)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def test_challenges(self):
-    #     # Test retrieving challenges
-    #     self.client.force_authenticate(user=self.user1)
-    #     response = self.client.get(reverse('get-challenges'))
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    # def tearDown(self):
-    #     # Clean up any created test data
-    #     User.objects.all().delete()
-    #     Geolocation.objects.all().delete()
-    #     Posts.objects.all().delete()
-    #     Stickers.objects.all().delete()
-    #     PostsUser.objects.all().delete()
-    #     Challenges.objects.all().delete()
-
+    
