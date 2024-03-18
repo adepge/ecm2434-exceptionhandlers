994a1e79f566250c255ffb404a9fea9192e5a43b
lyca499 <lyca499abc@gmail.com>
2024-03-18 17:46:54 +0000

    feed page fix

--- frontend/src/pages/feed.jsx
+++ frontend/src/pages/feed.jsx
@@ -36,7 +36,7 @@ function FeedPage() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/api/collectedPosts/",
+        "https://api.post-i-tivity.me/api/collectedPosts/",
         {},
         {
           headers: {
@@ -92,7 +92,7 @@ function FeedPage() {
 
       for (let i = 0; i < postList.length; i++) {
 
-        postList[i]["image"] = "http://127.0.0.1:8000/" + postList[i]["image"];
+        postList[i]["image"] = "https://api.post-i-tivity.me" + postList[i]["image"];
 
         const image = postList[i]["image"]
 
