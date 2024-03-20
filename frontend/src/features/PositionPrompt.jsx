48a38da010a1324e7317ee9e164426a650d6b9de
lyca499 <lyca499abc@gmail.com>
2024-03-20 22:31:03 +0000

    prompt position

--- frontend/src/features/PositionPrompt.jsx
+++ frontend/src/features/PositionPrompt.jsx
@@ -13,7 +13,6 @@ export default function PositionPrompt({ promptShown }) {
                 // reload the page to get the new location
                 // window.location.reload();
                 promptShown();
-                setShowPrompt(false);
             });
         }
     };
@@ -26,32 +25,29 @@ export default function PositionPrompt({ promptShown }) {
                 promptShown();
             } else {
                 setShowPrompt(true);
-                promptShown();
             }
         }
         check();
     }, [navigator.permissions.query({ name: "geolocation" })])
 
     return (
-        <>
-            <div className="position-prompt" style={{ display: show ? "" : "none" }}>
-                <div className="position-prompt-content" style={{ display: show ? "" : "none" }}>
-                    <div className='prompt'>
-                        <div className='icon'>
-                            <img src={Navation} alt="navigation" />
-                        </div>
-                        <div className='title'>
-                            Allow location access
-                        </div>
-                        <div>Location access is an integral part of the app. Please allow access to your location to continue.
-                        </div>
-
-                        <div className='guide'>Not working? <a href='https://support.google.com/chrome/answer/142065?hl=en-GB&co=GENIE.Platform%3DDesktop'>Troubleshooting guide</a></div>
-                        <button onClick={onclick}>Allow Access</button>
+        <div className="position-prompt" style={{ display: show ? "" : "none" }}>
+            <div className="position-prompt-content">
+                <div className='prompt'>
+                    <div className='icon'>
+                        <img src={Navation} alt="navigation" />
                     </div>
-                </div >
+                    <div className='title'>
+                        Allow location access
+                    </div>
+                    <div>Location access is an integral part of the app. Please allow access to your location to continue.
+                    </div>
+
+                    <div className='guide'>Not working? <a href='https://support.google.com/chrome/answer/142065?hl=en-GB&co=GENIE.Platform%3DDesktop'>Troubleshooting guide</a></div>
+                    <button onClick={onclick}>Allow Access</button>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
