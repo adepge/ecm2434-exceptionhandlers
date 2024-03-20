8f468f51b1abbcef330c66104681ce463e605476
lyca499 <lyca499abc@gmail.com>
2024-03-20 22:27:35 +0000

    reverted checklogin

--- frontend/src/features/CheckLogin.jsx
+++ frontend/src/features/CheckLogin.jsx
@@ -5,53 +5,47 @@ import Cookies from "universal-cookie";
 
 const cookies = new Cookies();
 
-function CheckLogin(redirect = true) {
-    const checkLogin = async () => {
-        if (cookies.get('token') === undefined) {
-            console.log("redirecting")
-            console.log(redirect);
-            if (redirect) {
-                window.location.href = "/login";
-            }
+const checkLogin = async () => {
+    if (cookies.get('token') === undefined) {
+        console.log("redirecting")
+        console.log(redirect);
+        if (redirect) {
+            window.location.href = "/login";
         }
+    }
 
-        try {
-            const response = await axios.get(
-                "https://api.post-i-tivity.me/api/getUser/"
-                ,
-                {
-                    headers: {
-                        "Content-Type": "multipart/form-data",
-                        "Authorization": `Token ${cookies.get('token')}`, // Assuming postData.username is the token
-                    },
-                }
-            );
-            return response;
-        } catch (error) {
-            if (error.response) {
-                if (error.response.data.detail === "Invalid token.") {
-                    cookies.remove('token');
-                    console.log("not logged in");
-                    if (redirect) {
-                        console.log("redirecting")
-                        window.location.href = "/login";
-                        return false
-                    }
-                } else {
-                    console.log("Response data:", error.response.data);
-                    console.log("Response status:", error.response.status);
-                    console.log("Response headers:", error.response.headers);
-                    alert("Internal server error");
+    try {
+        const response = await axios.get(
+            "https://api.post-i-tivity.me/api/getUser/"
+            ,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": `Token ${cookies.get('token')}`, // Assuming postData.username is the token
+                },
+            }
+        );
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.data.detail === "Invalid token.") {
+                cookies.remove('token');
+                console.log("not logged in");
+                if (redirect) {
+                    console.log("redirecting")
+                    window.location.href = "/login";
+                    return false
                 }
             } else {
-                console.log(error)
-                // alert("Cannot connect to the server(CheckLogin)", error);
+                console.log("Response data:", error.response.data);
+                console.log("Response status:", error.response.status);
+                console.log("Response headers:", error.response.headers);
+                alert("Internal server error");
             }
-            return false
+        } else {
+            console.log(error)
+            // alert("Cannot connect to the server(CheckLogin)", error);
         }
-    };
-
-    checkLogin();
-}
-
-export default CheckLogin;
+        return false
+    }
+};
