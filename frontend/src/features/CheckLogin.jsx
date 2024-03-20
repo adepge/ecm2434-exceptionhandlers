4bf4061c8284c16284ec9ec3eb7c047301b69ce6
Adam George <adepge@gmail.com>
2024-03-20 21:48:01 +0000

    Revert commit

--- frontend/src/features/CheckLogin.jsx
+++ frontend/src/features/CheckLogin.jsx
@@ -2,61 +2,60 @@
 
 import axios from "axios";
 import Cookies from "universal-cookie";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const cookies = new Cookies();
 
-function CheckLogin(redirect = true) {
+async function CheckLogin(redirect = true) {
+
+    // cookies.remove('token');
+
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const checkLogin = async () => {
-            if (cookies.get('token') === undefined) {
-            console.log("redirecting")
-            console.log(redirect);
-            if (redirect) {
-                navigate("/login");
-            }
-            }
+    if (cookies.get('token') === undefined) {
+        console.log("redirecting")
+        console.log(redirect);
+        if (redirect) {
+            navigate("/login");
+            return false;
+        }
+    }
 
-            try {
-                const response = await axios.get(
-                    "https://api.post-i-tivity.me/api/getUser/"
-                    ,
-                    {
-                        headers: {
-                            "Content-Type": "multipart/form-data",
-                            "Authorization": `Token ${cookies.get('token')}`, // Assuming postData.username is the token
-                        },
-                    }
-                );
-                return response;
-            } catch (error) {
-                if (error.response) {
-                    if (error.response.data.detail === "Invalid token.") {
-                        cookies.remove('token');
-                        console.log("not logged in");
-                        if (redirect) {
-                            console.log("redirecting")
-                            navigate("/login");
-                            return false
-                        }
-                    } else {
-                        console.log("Response data:", error.response.data);
-                        console.log("Response status:", error.response.status);
-                        console.log("Response headers:", error.response.headers);
-                        alert("Internal server error");
-                    }
-                } else {
-                    console.log(error)
-                    alert("Cannot connect to the server(CheckLogin)", error);
+    try {
+        const response = await axios.get(
+            "https://api.post-i-tivity.me/api/getUser/"
+            ,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": `Token ${cookies.get('token')}`, // Assuming postData.username is the token
+                },
+            }
+        );
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.data.detail === "Invalid token.") {
+                cookies.remove('token');
+                console.log("not logged in");
+                if (redirect) {
+                    console.log("redirecting")
+                    navigate("/login");
+                    return false
                 }
-                return false
+            } else {
+                console.log("Response data:", error.response.data);
+                console.log("Response status:", error.response.status);
+                console.log("Response headers:", error.response.headers);
+                alert("Internal server error");
             }
-        };
+        } else {
+            console.log(error)
+            alert("Cannot connect to the server(CheckLogin)", error);
+        }
+        return false
+    }
 
-        checkLogin();
-    }, [navigate, redirect]);
 }
 
 export default CheckLogin;
