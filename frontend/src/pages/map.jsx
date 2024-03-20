93da6eedcc6c3985675db212ab960451c4daadfa
lyca499 <lyca499abc@gmail.com>
2024-03-20 23:27:47 +0000

    map page

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useMemo } from "react";
 import { usePositionStore } from "../stores/geolocationStore";
 import { usePinStore, useCollectedPinStore } from "../stores/pinStore";
-import { useNavigate } from "@reach/router";
+import { useNavigate } from "react-router-dom";
 import "./stylesheets/map.css";
 import DrawerDown from "../features/DrawerDown";
 import axios from "axios";
