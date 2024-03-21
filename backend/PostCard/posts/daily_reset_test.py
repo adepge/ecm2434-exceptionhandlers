f73fdabfa6e4f95ad7366eb7428abaa7c653de07
ziyad <ziyad.alnawfal1@gmail.com>
2024-03-21 01:32:39 +0000

    changed dailyReset

--- backend/PostCard/posts/daily_reset_test.py
+++ backend/PostCard/posts/daily_reset_test.py
@@ -14,11 +14,14 @@ def daily_reset_test(request):
         x.postsMadeToday=0
         x.postsSavedToday=0
         x.save()
-    
-    for y in Challenges.objects.all()[0:1]:
+
+    num_challenges=0
+    for y in Challenges.objects.filter(type="daily"):
         y.inUse=False
+        num_challenges+=1
         y.save()
-    z=Challenges.objects.all()[random.randint(0,1)]
+        
+    z=Challenges.objects.filter(type="daily")[random.randint(0,num_challenges)]
     z.inUse=True
     z.save()
 
