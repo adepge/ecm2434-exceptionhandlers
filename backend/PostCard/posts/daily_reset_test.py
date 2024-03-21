e38899851affca66e6bbdfcfd976e4d8c214803b
lyca499 <lyca499abc@gmail.com>
2024-03-21 00:31:36 +0000

    daily reset test

---
+++ backend/PostCard/posts/daily_reset_test.py
@@ -0,0 +1,16 @@
+def daily_reset_test():
+    import random
+    from database.models import PostsUser, Challenges
+    random.seed()
+    for x in PostsUser.objects.all():
+        x.stepsTakenToday=0
+        x.postsMadeToday=0
+        x.postsSavedToday=0
+        x.save()
+    
+    for y in Challenges.objects.all()[0:1]:
+        y.inUse=False
+        y.save()
+    z=Challenges.objects.all()[random.randint(0,1)]
+    z.inUse=True
+    z.save()
